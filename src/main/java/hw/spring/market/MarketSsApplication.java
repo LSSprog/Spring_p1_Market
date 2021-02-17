1520e0a5b65013cc0df190f553e204f8ac5a9f9d
LSSprog <fedexss@bk.ru>
2021-02-17 16:27:01 +0300

    ДЗ к 3_15 уроку (first commit)

--- src/main/java/hw/spring/market/MarketSsApplication.java
+++ src/main/java/hw/spring/market/MarketSsApplication.java
@@ -8,6 +8,11 @@ import org.springframework.context.annotation.PropertySource;
 @PropertySource("classpath:secret.properties") //secret берётся не из yaml общего, а из отдельного файла, для безопасности
 public class MarketSsApplication {
 
+	// Домашнее задание:
+	// 1. Сделайте профилировщик, который подсчитывает какой метод в проекте
+	// используется чаще всего
+	// 2. * Попробуйте подсчитать методы какого контроллера уходит больше всего времени
+
 	public static void main(String[] args) {
 		SpringApplication.run(MarketSsApplication.class, args);
 	}
